5438455008e4178192e2569be01546a31963c65e
aloshai <arma.55@outlook.com>
2022-04-18 14:46:59 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+
+export default defineComponent({
+  components: {
+    RouterView
+  }
+});
+</script>
